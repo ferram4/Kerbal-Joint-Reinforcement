b0f829d4a25249c4e3d66815ba718dfd994e5172
ferram4 <ferram40@gmail.com>
2017-07-23 23:54:09 -0400

    Rebuild against KSP 1.3, make sure CompatChecker works

--- KerbalJointReinforcement/KerbalJointReinforcement/CompatibilityChecker.cs
+++ KerbalJointReinforcement/KerbalJointReinforcement/CompatibilityChecker.cs
@@ -64,7 +64,7 @@ namespace KerbalJointReinforcement
             // Even if you don't lock down functionality, you should return true if your users
             // can expect a future update to be available.
             //
-            return Versioning.version_major == 1 && Versioning.version_minor == 2;
+            return (Versioning.version_major == 1 && Versioning.version_minor == 3) || (Versioning.version_major == 0 && Versioning.version_minor == 1 && Versioning.Revision == 3);
 
             /*-----------------------------------------------*\
             | IMPLEMENTERS SHOULD NOT EDIT BEYOND THIS POINT! |
@@ -185,7 +185,7 @@ namespace KerbalJointReinforcement
 
             if ((incompatible.Length > 0) || (incompatibleUnity.Length > 0))// || IsWin64())
             {
-                PopupDialog.SpawnPopupDialog(new Vector2(0, 0), new Vector2(0, 0), "Incompatible Mods Detected", message, "OK", true, HighLogic.UISkin);
+                PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), "CompatibilityChecker", "Incompatible Mods Detected", message, "OK", true, HighLogic.UISkin);
             }
         }
 
